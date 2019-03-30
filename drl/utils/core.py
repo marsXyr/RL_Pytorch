c5322923f59a96de35b61e9f109bc018b1082277
marsXyr <33440299+marsXyr@users.noreply.github.com>
2019-03-30 20:58:13 +0800

    Add files via upload

---
+++ drl/utils/core.py
@@ -0,0 +1,101 @@
+import torch
+import numpy as np, random
+import gym
+from collections import namedtuple
+
+
+"""
+    use from pytorch deep reinforcement learning 
+"""
+Transition = namedtuple(
+    'Transition', ('state', 'action', 'next_state', 'reward', 'done'))
+
+
+class ReplayBuffer(object):
+
+    def __init__(self, buffer_size):
+        self.capacity = buffer_size
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+class OUNoise:
+    """
+    Ornstein-Uhnlenbeck process
+    """
+    def __init__(self, action_dimension, scale=0.3, mu=0, theta=0.15, sigma=0.2):
+        self.action_dimension = action_dimension
+        self.scale = scale
+        self.mu = mu
+        self.theta = theta
+        self.sigma = sigma
+        self.state = np.ones(self.action_dimension) * self.mu
+        self.reset()
+
+    def reset(self):
+        self.state = np.ones(self.action_dimension) * self.mu
+
+    def add(self):
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(len(x))
+        self.state = x + dx
+        return self.state * self.scale
+
+
+class GaussianNoise:
+    """
+    Simple Gaussian noise
+    """
+    def __init__(self, action_dim, sigma=0.2):
+        self.action_dim = action_dim
+        self.sigma = sigma
+
+    def add(self):
+        return np.random.normal(scale=self.sigma, size=self.action_dim)
+
+
+def to_numpy(var):
+    return var.data.numpy()
+
+
+def to_tensor(x, dtype='float'):
+    FloatTensor = torch.FloatTensor
+    LongTensor = torch.LongTensor
+    ByteTensor = torch.ByteTensor
+
+    if dtype == "float":
+        x = np.array(x, dtype=np.float64).tolist()
+        return FloatTensor(x)
+    elif dtype == "long":
+        x = np.array(x, dtype=np.long).tolist()
+        return LongTensor(x)
+    elif dtype == "byte":
+        x = np.array(x, dtype=np.byte).tolist()
+        return ByteTensor(x)
+    else:
+        x = np.array(x, dtype=np.float64).tolist()
+
+    return FloatTensor(x)
+
+
+def soft_update(target, source, tau):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
+
+
+def hard_update(target, source):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(param.data)
