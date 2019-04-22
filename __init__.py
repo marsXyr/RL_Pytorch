0a5a76975fc0f3aef59ced5ef5a0397a5b704cc0
marsXyr <33440299+marsXyr@users.noreply.github.com>
2019-04-22 21:10:09 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,13 @@
+# Algorithms
+from drl.algorithms.ddpg import DDPG
+# from spinup.algos.ppo.ppo import ppo
+# from spinup.algos.sac.sac import sac
+from drl.algorithms.td3 import TD3
+# from spinup.algos.trpo.trpo import trpo
+# from spinup.algos.vpg.vpg import vpg
+
+# Loggers
+from drl.utils.logx import Logger, EpochLogger
+
+# Version
+from drl.version import __version__
